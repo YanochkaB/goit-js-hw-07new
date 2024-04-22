c4abbfe365ffc845a6f6b0a9dccc6453cfa21bcd
YanochkaB <yanabukhtij@gmail.com>
2024-04-22 23:57:32 +0300

    started the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+list.addEventListener('click', gallery)
+
+//створення галереї
+const elem = galleryItems.map((oneElem) => {
+    const item = `<li class="gallery__item">
+    <a class="gallery__link" href="${oneElem.original}">
+    <img
+        class="gallery__image"
+        src="${oneElem.preview}"
+        data-source="${oneElem.original}"
+        alt="${oneElem.description}"
+    />
+    </a>
+</li>`;
+    return item
+}).join('')
+
+list.insertAdjacentHTML("afterbegin", elem);
+
+function gallery(event) {
+    event.preventDefault()
+  //перевірити чи клік відбувся по картинці (в основній ф-ції)
+  if (event.target.nodeName !== 'img') {
+      return
+  }
+}
