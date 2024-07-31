40875a027153ec9455327e5fd4976d1657e73817
YanochkaB <yanabukhtij@gmail.com>
2024-08-01 01:00:49 +0300

    start task two

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,3 +3,23 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
  
+const list = document.querySelector(".gallery"); //ul для галереї
+const cardsMarkup = creatImg(galleryItems);
+
+//створення галереї
+function creatImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}"
+       alt="${description}" />
+   </a>
+</li>`
+    })
+    .join("")
+}
+
+//відображення галереї (додавання в DOM)
+list.insertAdjacentHTML("afterbegin", cardsMarkup);
+
