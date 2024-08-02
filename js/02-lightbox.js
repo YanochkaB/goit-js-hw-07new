0086eca150385f6a452a5b3fab3efff8c8595171
YanochkaB <yanabukhtij@gmail.com>
2024-08-02 23:25:42 +0300

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
  
 const list = document.querySelector(".gallery"); //ul для галереї
 const cardsMarkup = creatImg(galleryItems);
